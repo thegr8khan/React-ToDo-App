d76bc0ec9e18b182b63f5bad4b23503b81db76ee
thegr8khan <moh.nkhan@outlook.com>
2025-07-08 05:53:46 -0500

    Added redirection to the welcome page

--- react-todo-app/src/components/todo/TodoApp.jsx
+++ react-todo-app/src/components/todo/TodoApp.jsx
@@ -1,13 +1,18 @@
-import './TodoApp.css';
 import React, { useState } from 'react';
-
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom';
+import './TodoApp.css';
 
 export default function TodoApp() {
   return (
     <div className="TodoApp">
-      To-Do Management Application
-      <LoginComponent />
-      {/* <WelcomeComponent /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginComponent />}></Route>
+          <Route path='/login' element={<LoginComponent />}></Route>
+          <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
+          <Route path='*' element={<ErrorComponent />}></Route>    
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
@@ -19,6 +24,8 @@ function LoginComponent() {
 
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+    const navigate = useNavigate();
 
     function handleUsernameChange(event) {
         setUsername(event.target.value);
@@ -33,6 +40,7 @@ function LoginComponent() {
             setShowSuccessMessage(true);
             setShowErrorMessage(false);
             console.log('Login Successful!');
+            navigate(`/welcome/${username}`);
         } else {
             setShowErrorMessage(true);
             setShowSuccessMessage(false);
@@ -42,6 +50,8 @@ function LoginComponent() {
     
     return (
         <div className="Login">
+          <h1>LOGIN</h1>
+            <p>Enter your credentials to login.</p>
             {showSuccessMessage && <div className='successMessage'>Login Successful!</div>}
             {showErrorMessage && <div className='errorMessage'>Login Failed! Please check your credentials.</div>}
             <div className="LoginForm">
@@ -64,9 +74,19 @@ function LoginComponent() {
 
 
 function WelcomeComponent() {
+  const {username} = useParams();
+  console.log(username);
   return (
-    <div className="Welcome">
-      Welcome Component
+    <div className="WelcomeComponent">
+      <h1>Welcome {username}</h1>
+    </div>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>An Error Occurred. Please try again later.</h1>
     </div>
   );
 }
